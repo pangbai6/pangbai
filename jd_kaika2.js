7fc28dd4c35d175d855b1054495780eb09d65a38
旁白 <94520057+pangbai6@users.noreply.github.com>
2022-02-08 11:42:41 +0800

    臻宠会员 甜蜜告白

--- jd_kaika2.js
+++ jd_kaika2.js
@@ -1,4 +1,4 @@
-const $ = new Env("大牌好礼带回家");
+const $ = new Env("臻宠会员 甜蜜告白");
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message = '';
